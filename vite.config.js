677710b16624820022659854dd45644ca3d57852
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-09-10 01:00:52 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const base =
+  process.env.NODE_ENV === "production" ? "/react-vite-ecommerce/" : "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-vite-ecommerce/",
+  base,
 });
