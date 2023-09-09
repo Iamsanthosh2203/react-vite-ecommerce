7e3e9fd6b0ce7935e291b998a50e4d849001198c
Santhosh Kumar <iamsanthosh2203@gmail.com>
2023-09-10 00:31:11 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,39 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Chair from "./pages/Chair.jsx";
-import Tabel from "./pages/Tabel.jsx";
-import Sofa from "./pages/Sofa.jsx";
-
-const route = createBrowserRouter([
-  {
-    path: "/react-vite-ecommerce/",
-    element: <App />,
-    children: [
-      {
-        path: "/react-vite-ecommerce/",
-        element: <Home />,
-      },
-      {
-        path: "/react-vite-ecommerce/chair",
-        element: <Chair />,
-      },
-      {
-        path: "/react-vite-ecommerce/tabl",
-        element: <Tabel />,
-      },
-      {
-        path: "/react-vite-ecommerce/sofa",
-        element: <Sofa />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={route} />
+    <App />
   </React.StrictMode>
 );
